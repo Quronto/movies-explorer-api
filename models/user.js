fa79280b8611f6e88086ae29f1dbd4ce7c6e47e5
Daniil Kuznetsov <dankuzi@yandex.ru>
2023-08-21 03:29:45 +0400

    создать модели и схемы для user и movie

---
+++ models/user.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Хеширование пароля перед сохранением пользователя
+userSchema.pre('save', async function(next) {
+  try {
+    if (!this.isModified('password')) {
+      return next();
+    }
+
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
