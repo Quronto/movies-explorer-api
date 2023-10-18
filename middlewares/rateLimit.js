e1c76dd80de6134147960bab37dbd94908913993
Daniil Kuznetsov <dankuzi@yandex.ru>
2023-10-19 01:40:25 +0400

    cors исправить

--- middlewares/rateLimit.js
+++ middlewares/rateLimit.js
@@ -2,7 +2,7 @@ const rateLimit = require('express-rate-limit');
 
 const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
-  max: 200,
+  max: 2000,
   message: 'Вы превысили лимит запросов, пожалуйста, повторите попытку позже.',
 });
 
